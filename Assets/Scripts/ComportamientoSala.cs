4c260c94c20b506ca604842613691dabb5e5948e
Manolo <manolojm96@gmail.com>
2023-03-30 12:20:38 +0200

    maze

--- Assets/Scripts/ComportamientoSala.cs
+++ Assets/Scripts/ComportamientoSala.cs
@@ -2,27 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComportamientoSala : MonoBehaviour
-{
-    public GameObject[] muros;
-    public GameObject[] puertas;
+public class ComportamientoSala : MonoBehaviour {
 
-    public void UpdateSala(bool[] status) {
-        for (int i = 0; i < status.Length; i++) {
-            puertas[i].SetActive(status[i]);
-            muros[i].SetActive(!status[i]);
-        }
-    }
+    public GameObject[] muro; // 0- top, 1- down, 2- left, 3- right
+    public GameObject[] puerta;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void UpdateSala(bool[] status) {
+        for (int i = 0; i < status.Length; i++) {
+            puerta[i].SetActive(status[i]);
+            muro[i].SetActive(!status[i]);
+        }
     }
 }
--- Assets/Scripts/GeneradorMazmorra.cs
+++ Assets/Scripts/GeneradorMazmorra.cs
@@ -1,65 +1,128 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeneradorMazmorra : MonoBehaviour
-{
+public class GeneradorMazmorra : MonoBehaviour {
+
     public class Celda {
         public bool visitada = false;
         public bool[] status = new bool[4];
     }
 
     public Vector2Int size;
-    public int posicionInicio = 0;
+    public int posInicio = 0;
     public GameObject sala;
-    public Vector2 dimension;
+    public Vector2Int offset;
 
     List<Celda> tablero;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        GeneradorLaberinto();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        GeneraLaberinto();
     }
 
     void GeneraMazmorra() {
         for (int i = 0; i < size.x; i++) {
             for (int j = 0; j < size.y; j++) {
-                //var nuevaSala = Instantiate(sala, new Vector3(i * dimension.x, 0, j * dimension.y), Quaternion.identity, transform).GetComponent<ComportamientoSala>;
-                //nuevaSala.UpdateSala(tablero[i + j * size.x].status);
+                Celda celdaActual = tablero[i + j * size.x];
+                if (celdaActual.visitada) {
+                    var nuevaSala = Instantiate(sala, new Vector3(i * offset.x, 0, j * offset.y), Quaternion.identity, transform).GetComponent<ComportamientoSala>();
+                    nuevaSala.UpdateSala(tablero[i + j * size.x].status);
+                }
             }
         }
     }
 
-    void GeneradorLaberinto() {
+    void GeneraLaberinto() {
         tablero = new List<Celda>();
+
         for (int i = 0; i < size.x; i++) {
-            for (int j = 0; i < size.y; i++) {
+            for (int j = 0; j < size.y; j++) {
                 tablero.Add(new Celda());
             }
         }
 
-        int celdaActual = posicionInicio;
+        int celdaActual = posInicio;
+
         Stack<int> camino = new Stack<int>();
+
         int num = 0;
-        while(num < 100) {
+
+        while (num < 20) {
             num++;
+
+            tablero[celdaActual].visitada = true;
+
+
+            // Comprbobamos los vecinos
+            List<int> vecinos = CompruebaVecinos(celdaActual);
+
+            if (vecinos.Count == 0) {
+                if (camino.Count == 0) {
+                    break;
+                } else {
+                    celdaActual = camino.Pop();
+                }
+            } else {
+                camino.Push(celdaActual);
+
+                int nuevaCelda = vecinos[Random.Range(0, vecinos.Count)];
+
+                if (nuevaCelda > celdaActual) {
+                    // Abajo o derecha
+                    if (nuevaCelda - 1 == celdaActual) {
+                        tablero[celdaActual].status[3] = true;
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[2] = true;
+                    } else {
+                        tablero[celdaActual].status[0] = true;
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[1] = true;
+                    }
+                } else {
+                    // Arriba o izquierda
+                    if (nuevaCelda + 1 == celdaActual) {
+                        tablero[celdaActual].status[2] = true;
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[3] = true;
+                    } else {
+                        tablero[celdaActual].status[1] = true;
+                        celdaActual = nuevaCelda;
+                        tablero[celdaActual].status[0] = true;
+                    }
+                }
+            }
         }
+
+        GeneraMazmorra();
     }
 
-    void CompruebaVecinos(int celda) {
+
+    List<int> CompruebaVecinos(int celda) {
         List<int> vecinos = new List<int>();
 
-        // Comprobar vecinos de arriba
-        if (celda - size.x >= 0 && !tablero[celda - size.x].visitada) {
+        // Comprobamos vecino de arriba
+        if (celda - size.x >= 0 && !tablero[(celda - size.x)].visitada) {
             vecinos.Add(celda - size.x);
         }
-        
+
+
+        // comprobamos vecino de abajo
+        if (celda + size.x < tablero.Count && !tablero[celda + size.x].visitada) {
+            vecinos.Add((celda + size.x));
+        }
+
+        // combobamos vecino de la derecha
+        if ((celda + 1) % size.x != 0 && !tablero[celda + 1].visitada) {
+            vecinos.Add(celda + 1);
+        }
+
+        // Comprobamos el vecino de la izquieda
+        if ((celda) % size.x != 0 && !tablero[celda - 1].visitada) {
+            vecinos.Add(celda - 1);
+        }
+
+        return vecinos;
     }
 }
